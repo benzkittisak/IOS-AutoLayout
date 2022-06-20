47826413dd3b18cf71ae7caa101917ea5459ddc3
benzkittisak <benzkittisak@stv.ac.th>
2022-06-20 12:54:15 +0700

    use constrain , stack layout in application

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceIV1: UIImageView!
+    @IBOutlet weak var diceIV2: UIImageView!
+    
+    var imageSet = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnRoll(_ sender: UIButton) {
+        diceIV1.image = imageSet.randomElement()
+        diceIV2.image = imageSet.randomElement()
+    }
 }
 
